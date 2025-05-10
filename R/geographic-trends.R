58a71e71f3f3e08c34ac54346aa21151703642a5
Jadon Calvert <calvertjadon@gmail.com>
2025-05-10 14:39:35 -0400

    add mean severity by state map plot

--- R/geographic-trends.R
+++ R/geographic-trends.R
@@ -1,8 +1,25 @@
 # Geographic trends: If you’re comfortable, you could focus on the heat map by
 #    state and the environmental factors analysis (weather vs. road conditions).
 
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(here)
+library(readr)
+library(ggiraphExtra)
+library(maps)
+library(mapproj)
+library(stringr)
+
+if(!exists("accidents")) {
+  #accidents <- read.csv("data/raw_data/US_Accidents_March23.csv")
+  accidents <- read_rds(
+    here("data", "processed_data", "US_Accidents_March23.rds")
+  )
+}
+
 meanseverity_state <- accidents %>% 
-  group_by(State) %>% 
+  group_by(State, state_name) %>% 
   summarise(mean_severity = mean(Severity)) %>% 
   arrange(desc(mean_severity))
 meanseverity_state
@@ -23,3 +40,13 @@ meanseverity_state_p <- ggplotly(ggplot(
   tooltip = c("x", "y")
 )
 meanseverity_state_p
+
+states_map <- map_data("state")
+str(states_map)
+states_map$region <- str_to_title(states_map$region)
+
+ggChoropleth(data = meanseverity_state,
+             aes(fill = mean_severity,
+                 map_id = state_name),
+             map = states_map,
+             interactive = T)
--- R/prep.R
+++ R/prep.R
@@ -1,8 +1,9 @@
 library(readr)
 library(here)
 library(dplyr)
+library(stringr)
 
-if(!exists("accidents")) {
+if(!exists("accidents_raw")) {
   #accidents <- read.csv("data/raw_data/US_Accidents_March23.csv")
   accidents_raw <- read_csv(
     here("data", "raw_data", "US_Accidents_March23.csv"),
@@ -15,11 +16,20 @@ dim(accidents_raw)
 str(accidents_raw)
 summary(accidents_raw)
 
+# make sure no na in severity
 table(is.na(accidents_raw$Severity))
+
+# lookup table to map state codes to state names (for choropleth)
+state_lookup <- tibble(
+  State  = state.abb,
+  state_name  = str_to_title(state.name)
+)
 
 accidents <- accidents_raw %>% 
   mutate(sevg = ifelse(Severity == 1, "least severe",
                        ifelse(Severity == 2, "less severe",
-                              ifelse(Severity == 3, "more severe", "most severe"))))
+                              ifelse(Severity == 3, "more severe", "most severe")))) %>% 
+  inner_join(state_lookup, by = "State")
+str(accidents %>% select(State, state_name))
 
 write_rds(accidents, here("data", "processed_data", "US_Accidents_March23.rds"))
