9f442c84357fe10d0c2e08a94e1127463b3a6a30
Jadon Calvert <calvertjadon@gmail.com>
2025-05-10 20:17:02 -0400

    update geo to use shorter acc variable name

--- R/geographic-trends.R
+++ R/geographic-trends.R
@@ -11,42 +11,42 @@ library(maps)
 library(mapproj)
 library(stringr)
 
-if(!exists("accidents")) {
+if(!exists("acc")) {
   #accidents <- read.csv("data/raw_data/US_Accidents_March23.csv")
-  accidents <- read_rds(
+  acc <- read_rds(
     here("data", "processed_data", "US_Accidents_March23.rds")
   )
 }
 
-meanseverity_state <- accidents %>% 
+# SEVERITY BY STATE
+meanseverity_state <- acc %>% 
   group_by(State, state_name) %>% 
   summarise(mean_severity = mean(Severity)) %>% 
   arrange(desc(mean_severity))
 meanseverity_state
 
-meanseverity_state_p <- ggplotly(ggplot(
-  data = meanseverity_state,
-  aes(x = factor(State, levels = State),
-      y = mean_severity,
-      fill = mean_severity)
-  ) +
-  geom_bar(stat = "identity", position = "dodge") +
+p <- ggplot(data = meanseverity_state,
+            aes(x = reorder(State, -mean_severity),
+                y = mean_severity)) +
+  geom_col(position = "dodge") +
   coord_flip() + 
   labs(
     title = "Average Accident Severity by State (2016 – 2023)",
     x = NULL,
     y = "Mean Severity (1 = least, 4 = most)"
-  ),
-  tooltip = c("x", "y")
-)
+  )
+
+meanseverity_state_p <- ggplotly(p, tooltip = c("x", "y"))
 meanseverity_state_p
 
 states_map <- map_data("state")
 str(states_map)
 states_map$region <- str_to_title(states_map$region)
+str(states_map)
 
-ggChoropleth(data = meanseverity_state,
+meanseverity_state_p2 <- ggChoropleth(data = meanseverity_state,
              aes(fill = mean_severity,
                  map_id = state_name),
              map = states_map,
              interactive = T)
+meanseverity_state_p2
